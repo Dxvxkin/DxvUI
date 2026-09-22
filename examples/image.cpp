87441a9d22a60a3f0362e17387a55a69b0841a8a
Dxvxkin <ponch1397@gmail.com n>
2026-09-22 20:08:29 +0300

    fix(example): image — безымянные argc/argv ломали сборку SDL_main на Windows

--- examples/image.cpp
+++ examples/image.cpp
@@ -214,7 +214,7 @@ class DxvUIImageExampleApp : public DxvUIImageExample {
 };
 
 #ifdef _WIN32
-extern "C" int SDL_main(int /*argc*/, char* /*argv*/[]) {
+extern "C" int SDL_main(int argc, char* argv[]) {
 #else
 int main(int argc, char* argv[]) {
 #endif
